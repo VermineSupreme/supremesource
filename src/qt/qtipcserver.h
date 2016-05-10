17ae03261f5465f9af3c0734b16078ad59657450
Supreme <pulsaricormaster@gmail.com>
2016-05-10 09:23:40 -0400

    Supremesource
    source supreme

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Supreme-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SupremeURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
